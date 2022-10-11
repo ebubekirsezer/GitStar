d127a0077475373ec00a81caa9ddfcf0e15865ec
Ebubekir Sezer <ebubekirssezer@gmail.com>
2022-10-11 14:44:24 +0300

    Project initially created

---
+++ GitStar/GitStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitStarApp.swift
+//  GitStar
+//
+//  Created by EbubekirSezer on 11.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct GitStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
